3bd659673c8275fefb0c9a4e214d2dda306bef7f
Ramakant sharma <102178267+ramakant822382@users.noreply.github.com>
2026-08-18 00:53:43 +0530

    versal add

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://eco-avenger.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
